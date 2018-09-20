dffa193a0dc3dacad4e4bfcfb384afceb5fea7c8
Gagan <gagansaini6164@gmail.com>
2018-09-20 23:50:59 +0530

    added bootstap

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import ReactDom from 'react-dom';
+import React from "react";
+import ReactDom from "react-dom";
+import "bootstrap/dist/css/bootstrap.css";
 
-const element = <h1>Hello world!</h1>
-ReactDom.render(element, document.getElementById('root'))
+const element = <h1>Hello world!</h1>;
+ReactDom.render(element, document.getElementById("root"));
