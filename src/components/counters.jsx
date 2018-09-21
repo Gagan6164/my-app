c64da62cd934a0405fbfd767c970f6ea1501a1c2
Gagan <gagansaini6164@gmail.com>
2018-09-21 12:37:08 +0530

    added count functionality

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -1,14 +1,28 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state2 = {
+    tags: ["learn react", "complete week 4 on coursera", "start ML"]
+  };
   state = {
-    count: 0,
-    tags: ["tag1", "tag2", "tag3"]
+    count: this.state2.tags.length
   };
   styles = {
     fontSize: 20,
     fontWeight: "bold"
   };
+
+  renderTags() {
+    if (this.state2.tags.length === 0) return <p>you todo list is empty</p>;
+
+    return (
+      <ul>
+        {this.state2.tags.map(tag => (
+          <li key={tag}>{tag}</li>
+        ))}
+      </ul>
+    );
+  }
 
   render() {
     return (
@@ -16,12 +30,9 @@ class Counter extends Component {
         <span style={this.styles} className={this.getBadgeMethod()}>
           {this.formatCount()}
         </span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
-        <ul>
-          {this.state.tags.map(tag => (
-            <li>{tag}</li>
-          ))}
-        </ul>
+        <button className="btn btn-secondary btn-sm">Add</button>
+        {this.state2.tags.length === 0 && "please add items to your todo list"}
+        {this.renderTags()}
       </div>
     );
   }
@@ -33,7 +44,7 @@ class Counter extends Component {
 
   formatCount() {
     const { count } = this.state;
-    return count === 0 ? "Zero" : count;
+    return count === 0 ? "No things TO do" : count;
   }
 }
 
