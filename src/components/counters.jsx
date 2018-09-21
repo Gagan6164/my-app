c5e580e5cd24c27f13d405c62d53aed38ea1679f
Gagan <gagansaini6164@gmail.com>
2018-09-21 12:15:15 +0530

    refacter method  with dynamic rendering

--- src/components/counters.jsx
+++ src/components/counters.jsx
@@ -3,21 +3,37 @@ import React, { Component } from "react";
 class Counter extends Component {
   state = {
     count: 0,
-    imageUrl: "https://picsum.photos/200"
+    tags: ["tag1", "tag2", "tag3"]
+  };
+  styles = {
+    fontSize: 20,
+    fontWeight: "bold"
   };
 
   render() {
     return (
       <div>
-        <img src={this.state.imageUrl} alt="" />
-        <span>{this.formatCount()}</span>
-        <button>Increment</button>
+        <span style={this.styles} className={this.getBadgeMethod()}>
+          {this.formatCount()}
+        </span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
+        <ul>
+          {this.state.tags.map(tag => (
+            <li>{tag}</li>
+          ))}
+        </ul>
       </div>
     );
   }
+  getBadgeMethod() {
+    let classes = "badge m-2 badge-";
+    classes += this.state.count === 0 ? "warning" : "primary";
+    return classes;
+  }
+
   formatCount() {
     const { count } = this.state;
-    return count === 0 ? 'Zero' : count;
+    return count === 0 ? "Zero" : count;
   }
 }
 
